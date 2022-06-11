8a17b7f2cb4add0f3e33956a136ceca4d5867c3a
kaeru411r <ike269fr@gmail.com>
2022-06-11 16:28:59 +0900

    エネミーが湧くように

---
+++ Assets/Scripts/Data/GameData.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData
+{
+    float _baseAtk;
+    /// <summary></summary>
+    float _baseDef;
+    /// <summary></summary>
+    float _baseMaxHP;
+    /// <summary></summary>
+    float _baseRecovery;
+    /// <summary></summary>
+    float _baseCT;
+    /// <summary></summary>
+    float _baseAttackArea;
+    /// <summary></summary>
+    float _baseAttackSpeed;
+    /// <summary></summary>
+    float _baseDuration;
+    /// <summary></summary>
+    float _baseAmount;
+    /// <summary></summary>
+    float _baseMoveSpeed;
+    /// <summary></summary>
+    float _baseCollectionArea;
+    /// <summary></summary>
+    float _baseLuck;
+    /// <summary></summary>
+    float _baseExpFact;
+    /// <summary></summary>
+    float _baseDifficulty;
+    /// <summary></summary>
+    float _base;
+    ///// <summary></summary>
+    //float _base;
+    ///// <summary></summary>
+    //float _base;
+    ///// <summary></summary>
+    //float _base;
+    ///// <summary></summary>
+    //float _base;
+    ///// <summary></summary>
+    //float _base;
+}
--- Assets/Scripts/Managers/EnemysManager.cs
+++ Assets/Scripts/Managers/EnemysManager.cs
@@ -10,12 +10,80 @@ public class EnemysManager : MonoBehaviour
     static public EnemysManager Instance;
 
 
-
+    [Tooltip("エネミーの限界量")]
+    [SerializeField] int _maxEnemyAmount;
     [Tooltip("エネミーのステータスと出現率を入れる配列を時間分に入れる配列")]
     [SerializeField] List<DegreeEnemySpawn> _enemySpawnList;
+    [Tooltip("エネミーの原型")]
+    [SerializeField] Enemy _baseEnemy;
+
+    /// <summary>有効なエネミーのリスト</summary>
+    List<Enemy> _activeEnemyList = new List<Enemy>();
+    /// <summary>無効なエネミーのリスト</summary>
+    List<Enemy> _inactiveEnemyList = new List<Enemy>();
+
+    float _enemySpawnTime;
+
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        EnemySetUp();
+    }
+
+    private void Update()
+    {
+        EnemyGenerate();
+    }
+
+    /// <summary>
+    /// エネミーの下準備をする
+    /// </summary>
+    void EnemySetUp()
+    {
+        for(int i = 0; i < _maxEnemyAmount; i++)
+        {
+            Enemy e = Instantiate(_baseEnemy);
+            e.SetUp();
+            _inactiveEnemyList.Add(e);
+        }
+    }
+
+    /// <summary>
+    /// エネミーが分ける条件を満たしているかチェックし、満たしていれば湧かす
+    /// </summary>
+    void EnemyGenerate()
+    {
+        int degree = Mathf.Min(_enemySpawnList.Count - 1, GameManager.Instance.Degree);
+        if (_enemySpawnTime > _enemySpawnList[degree].Time)
+        {
+            //Debug.Log();
+            if (_enemySpawnList[degree].MaxAmount > _activeEnemyList.Count)
+            {
+                if (_inactiveEnemyList.Count > 0)
+                {
+                    Debug.Log(3);
+                    EnemyInstantiate(Vector3.zero);
+                    _enemySpawnTime = 0;
+                }
+            }
+        }
+
+        _enemySpawnTime += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// エネミーを湧かす
+    /// </summary>
+    /// <param name="position"></param>
+    void EnemyInstantiate(Vector3 position)
+    {
+        _inactiveEnemyList[0].Instantiate(position);
+        _activeEnemyList.Add(_inactiveEnemyList[0]);
+        _inactiveEnemyList.RemoveAt(0);
     }
 
     /// <summary>
@@ -27,20 +95,20 @@ public class EnemysManager : MonoBehaviour
     {
         NullCheck();
         var data = _enemySpawnList[Mathf.Min(degree, _enemySpawnList.Count - 1)];
-        if(data.EnemySpawnsList.Count == 0)
+        if (data.EnemySpawnsList.Count == 0)
         {
             _enemySpawnList.Remove(data);
         }
-        int all =  0;
-        foreach(var e in data.EnemySpawnsList)
+        int all = 0;
+        foreach (var e in data.EnemySpawnsList)
         {
             all += e.Probability;
         }
         int num = Random.Range(0, all);
-        for(int i = 0; i < data.EnemySpawnsList.Count; i++)
+        for (int i = 0; i < data.EnemySpawnsList.Count; i++)
         {
             num -= data.EnemySpawnsList[i].Probability;
-            if(num < 0)
+            if (num < 0)
             {
                 return data.EnemySpawnsList[i].Data;
             }
@@ -54,10 +122,10 @@ public class EnemysManager : MonoBehaviour
     /// </summary>
     void NullCheck()
     {
-        for(int i = 0; i < _enemySpawnList.Count; i++)
+        for (int i = 0; i < _enemySpawnList.Count; i++)
         {
             _enemySpawnList[i].NullCheck();
-            if(_enemySpawnList[i].EnemySpawnsList == null || _enemySpawnList[i].EnemySpawnsList.Count == 0)
+            if (_enemySpawnList[i].EnemySpawnsList == null || _enemySpawnList[i].EnemySpawnsList.Count == 0)
             {
                 _enemySpawnList.RemoveAt(i);
                 i--;
@@ -89,16 +157,18 @@ struct DegreeEnemySpawn
     [Tooltip("インスペクター上で二次元配列の表示を実現するための型")]
     public List<EnemySpawn> EnemySpawnsList;
     [Tooltip("この段階で湧く敵の最大値")]
-    public int MaxNumber;
+    public int MaxAmount;
+    [Tooltip("敵の湧く頻度(秒)")]
+    public float Time;
 
     /// <summary>
     /// 配列内のdataにNullが合ったら削除
     /// </summary>
     public void NullCheck()
     {
-        for(int i = 0; i < EnemySpawnsList.Count; i++)
+        for (int i = 0; i < EnemySpawnsList.Count; i++)
         {
-            if(EnemySpawnsList[i].Data == null)
+            if (EnemySpawnsList[i].Data == null)
             {
                 EnemySpawnsList.RemoveAt(i);
             }
