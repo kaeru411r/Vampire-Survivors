e34b892adcc16dd813328098796f9a242ef29d9e
kaeru411r <ike269fr@gmail.com>
2022-06-17 04:12:53 +0900

    エネミーの落とす経験値を抽選できるように

--- Assets/Scripts/Data/EnemyData.cs
+++ Assets/Scripts/Data/EnemyData.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 
 /// <summary>
@@ -11,5 +12,29 @@ public class EnemyData : MonoBehaviour
     public float Atk;
     public float Radius;
     public Sprite Sprite;
+    public float Exp
+    {
+        get
+        {
+            int all = ExpTable.Sum(e => e.Probability);
+            int value = Random.Range(0, all + 1);
+            for (int i = 0; i < ExpTable.Length; i++)
+            {
+                if (value < ExpTable[i].Probability)
+                {
+                    return ExpTable[i].Exp;
+                }
+                value -= ExpTable[i].Probability;
+            }
+            return 0;
+        }
+    }
+    public ExpTable[] ExpTable;
+}
+
+[System.Serializable]
+public class ExpTable
+{
     public float Exp;
+    public int Probability;
 }
